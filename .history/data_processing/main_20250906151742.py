1738fa767f9f76e95b9c92bfc0aa4affcf92d162
LernerLior <166855348+LernerLior@users.noreply.github.com>
2025-09-06 16:13:03 -0300

    Delete .history directory

--- .history/data_processing/main_20250906151742.py
+++
@@ -1,8 +0,0 @@
-from crawler import collect_complaints
-from utils import *
-
-
-if __name__ == "__main__":
-        chromedriver_path = "C:/Users/Lior Lerner/Downloads/chromedriver-win64/chromedriver.exe"
-        excel_path = "santander_complaints.xlsx"
-        collect_complaints("santander", excel_path, chromedriver_path)
